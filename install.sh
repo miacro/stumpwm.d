6f746db2e2fe3dd154b8163a80f6bea0f7730dd0
miacro <fqguozhou@gmail.com>
2016-12-12 23:09:42 +0800

    ...

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-modules_path=${HOME}/.stumpwm.d/modules
-contrib_path=${HOME}/.stumpwm.d/modules/stumpwm-contrib
 sbcl --eval "(ql:quickload :clx)" --quit
 sbcl --eval "(ql:quickload :cl-ppcre)" --quit
+
+cache_dir=${HOME}/.stumpwm.d/.cache
+cache_stumpwm_dir=${cache_dir}/stumpwm
+modules_path=${HOME}/.stumpwm.d/modules
+contrib_path=${modules_path}/stumpwm-contrib
+mkdir -p ${cache_dir}
+if [[ -d ${cache_stumpwm_dir}/.git ]]
+then
+  cd ${cache_stumpwm_dir} && git pull
+else
+  git clone https://github.com/stumpwm/stumpwm ${cache_stumpwm_dir}
+fi
+
+cd ${cache_stumpwm_dir}
 autoconf
 ./configure --prefix=${HOME}/.stumpwm.d --with-module-dir=${HOME}/.stumpwm.d/modules
 make && make install 
